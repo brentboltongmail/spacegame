72b34838f773f4bfee8bede5cb3f19307e099cad
brentboltongmail <brentbolton@gmail.com>
2026-08-30 10:31:15 -0400

    Change hanger model X scale to 0.35

--- js/engine/models.js
+++ js/engine/models.js
@@ -115,7 +115,7 @@
                     }
 
                     // Reduce Y height by 40% (0.25 * 0.6 = 0.15)
-                    hangerModel.scale.set(0.25, 0.15, 0.25);
+                    hangerModel.scale.set(0.35, 0.15, 0.25);
                     // Move it closer to center (X=0.38) and raise it up to touch bottom of ring (Y=0)
                     hangerModel.position.set(0.38, 0, 0);
                     // Rotate the hanger so its opening faces outward from the center
